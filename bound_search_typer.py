08ae14db9465d749bc567a17c9c93472d4885a31
leejunghoon <coronarita1991@gmail.com>
2023-12-21 14:52:40 +0900

    Modify the code, give print option to lower/upper bound function, not to print at count_num function

--- bound_search_typer.py
+++ bound_search_typer.py
@@ -25,27 +25,45 @@ def lower_bound(target: Annotated[int, typer.Option(help="The number want to fin
                  lst: Annotated[str, 
                                 typer.Option(..., help="The list what you guess the target exists, for example : \"1 2 2 2 3 5\", \"[1, 2, 2, 2, 3, 5]\"",
                                              callback=parse_list)],
-                 
+                 print_result: bool = True
                  ):
     """ 
     Find the first index of target in list.
     """
     lower = lambda x, elem: x <= elem
     lo = bound_search(target, lst, lower)
-    print(lo)
+    if print_result :
+        print(lo)
+    return lo
     
 @app.command()
 def upper_bound(target: Annotated[int, typer.Option(help="The number want to find in list. For example, 2")],
                  lst: Annotated[str,
                                 typer.Option(..., help="The list what you guess the target exists, for example : \"1 2 2 2 3 5\", \"[1, 2, 2, 2, 3, 5]\"",
                                              callback=parse_list)],
+                 print_result: bool = True
                  ):
     """ 
     Find the last index + 1 of target in list.
     """
     upper = lambda x, elem: x < elem
     lo = bound_search(target, lst, upper)
-    print(lo)
+    if print_result :
+        print(lo)
+    return lo
+
+@app.command()
+def count_num(target: Annotated[int, typer.Option(help="The number want to find in list. For example, 2")],
+                 lst: Annotated[str,
+                                typer.Option(..., help="The list what you guess the target exists, for example : \"1 2 2 2 3 5\", \"[1, 2, 2, 2, 3, 5]\"",
+                                             callback=parse_list)],):
+    """
+    Return the number of target in the list.
+    """
+    cnt = upper_bound(target, lst, False) - lower_bound(target, lst, False)
+    print(cnt)
+    return cnt
+    
 
 if __name__ == "__main__":
     app()
